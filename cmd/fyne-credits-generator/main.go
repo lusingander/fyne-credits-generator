1c87fcfc75498c77706681548f5a8426417c02f0
fujimoto kyosuke <kyoro.f@gmail.com>
2020-04-18 18:07:20 +0900

    Add options
    - `-version`
    - `-help`

--- cmd/fyne-credits-generator/main.go
+++ cmd/fyne-credits-generator/main.go
@@ -1,13 +1,44 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 
 	credit "github.com/lusingander/fyne-credits-generator"
 )
 
+var (
+	version *bool
+	help    *bool
+)
+
+func parseFlags() {
+	version = flag.Bool("version", false, "print version")
+	help = flag.Bool("help", false, "print help")
+	flag.Parse()
+}
+
+func printHelp() {
+	fmt.Fprintln(os.Stderr, "Usage: fyne-credits-generator [flags]")
+	flag.PrintDefaults()
+}
+
+func printVersion() {
+	fmt.Fprintf(os.Stderr, "fyne-credits-generator version %s\n", credit.Version)
+}
+
 func run() error {
+	if *help {
+		printHelp()
+		return nil
+	}
+	if *version {
+		printVersion()
+		return nil
+	}
+
 	credits, err := credit.Collect()
 	if err != nil {
 		return err
@@ -17,6 +48,7 @@ func run() error {
 }
 
 func main() {
+	parseFlags()
 	if err := run(); err != nil {
 		log.Fatal(err)
 	}
